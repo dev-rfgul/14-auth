f32959440a6e6e2b18677caae0548b9047404be5
dev-rfgul <rfgul587@gmail.com>
2024-09-21 13:19:01 +0500

    completed till bcrypt

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 
+const bcrypt = require('bcrypt');
+
 
 app.use(cookieParser());
 app.get('/', (req, res) => {
@@ -13,11 +15,32 @@ app.get('/', (req, res) => {
 })
 app.get('/read', (req, res) => {
 
-    
+
     res.send('this is read page');
     /// to read the cookie we can use req.cookie and to send the cookie we can use res.cookie
     console.log(req.cookies);
 
+})
+
+app.get('/password', (req, res) => {
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash("password", salt, function (err, hash) {
+            console.log(hash);
+            res.send(hash);
+            // Store hash in your password DB.
+        });
+    });
+})
+
+app.get('/compare', (req, res) => {
+    bcrypt.compare('password', '$2b$10$8Y2g5IP.Gw1fJ8N/b9J4ReOUfrzlT3NOlKd5pDAtiSLh/ktszb7tq', function (err, result) {
+        if (result == true) {
+            res.send('password matched');
+        }
+        else {
+            res.send('password not matched');
+        }
+    });
 })
 
 
